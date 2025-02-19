a0129b4d7ecf62d2e80a4986334510b308556b0b
Patopy <patarguello2004@gmail.com>
2025-02-19 19:15:10 -0300

    fch 2025-02-19  agregue choices en models Clientes

--- AppClientes/models.py
+++ AppClientes/models.py
@@ -2,8 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Cliente(models.Model):
+    OP_SEXO = [
+        ("F", "Femenino"),
+        ("M", "Masculino"),
+        ("I", "Indefinido")
+    ]
+        
     nombre = models.CharField(max_length=30)
     edad   = models.IntegerField(blank=True, null=True)
+    sexo   = models.CharField(max_length=1, choices=OP_SEXO, default='I')
 
     def __str__(self):
         return self.nombre
+
+    class Meta:
+        db_table = 'tb_cliente'
+        ordering = ["nombre"]
+    
